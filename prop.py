4ac7dc42c204e811e199a191ff69ec76f495ead6
zip1982b <zip1982b@gmail.com>
2022-08-22 20:25:14 +0200

    add service discovery function, after connect()

--- prop.py
+++ prop.py
@@ -13,6 +13,40 @@ dbus.Dictionary(
         },
         signature=dbus.Signature('sv'))
 
+received signal
+path = /org/bluez/hci0/dev_6D_9E_14_7C_30_26/service0001 #путь к объекту (правое окно в d-feet)
+interfaces = dbus.Dictionary(
+{
+dbus.String('org.freedesktop.DBus.Introspectable'): dbus.Dictionary({}, signature=dbus.Signature('sv')),
+dbus.String('org.bluez.GattService1'): 
+                    dbus.Dictionary(
+                        {dbus.String('UUID'): dbus.String('00001801-0000-1000-8000-00805f9b34fb', variant_level=1),
+                         dbus.String('Device'): dbus.ObjectPath('/org/bluez/hci0/dev_6D_9E_14_7C_30_26', variant_level=1),
+                         dbus.String('Primary'): dbus.Boolean(True, variant_level=1),
+                         dbus.String('Includes'): dbus.Array([], signature=dbus.Signature('o'), variant_level=1)
+                        }, signature=dbus.Signature('sv')
+                        ),
 
+dbus.String('org.freedesktop.DBus.Properties'): dbus.Dictionary({}, signature=dbus.Signature('sv'))
+}, signature=dbus.Signature('sa{sv}')
+)
+end
+
+received signal
+path = /org/bluez/hci0/dev_6D_9E_14_7C_30_26/service0001/char0002
+interfaces = dbus.Dictionary(
+        {dbus.String('org.freedesktop.DBus.Introspectable'): dbus.Dictionary({}, signature=dbus.Signature('sv')),
+        dbus.String('org.bluez.GattCharacteristic1'): 
+            dbus.Dictionary(
+                {dbus.String('UUID'): dbus.String('00002a05-0000-1000-8000-00805f9b34fb', variant_level=1), 
+                dbus.String('Service'): dbus.ObjectPath('/org/bluez/hci0/dev_6D_9E_14_7C_30_26/service0001', variant_level=1),
+                dbus.String('Value'): dbus.Array([], signature=dbus.Signature('y'), variant_level=1), 
+                dbus.String('Notifying'): dbus.Boolean(False, variant_level=1),
+                dbus.String('Flags'): dbus.Array([dbus.String('indicate')], signature=dbus.Signature('s'), variant_level=1)
+                }, signature=dbus.Signature('sv')), 
+        dbus.String('org.freedesktop.DBus.Properties'): dbus.Dictionary({}, signature=dbus.Signature('sv'))
+        }, signature=dbus.Signature('sa{sv}')
+        )
+end
 
 
---
+++ service_discovery.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+import bluetooth_constants
+import bluetooth_utils
+import dbus
+import sys
+import time
+import dbus.mainloop.glib
+from gi.repository import GLib
+mainloop = None
+
+
+sys.path.insert(0, '.')
+bus = None
+device_interface = None
+
+
+def signal_received(path, interfaces):
+    print('received signal')
+    print(f'path = {path}')
+    print(f'interfaces = {interfaces}')
+
+
+def connect():
+    global bus
+    global device_interface
+
+    try:
+        device_interface.Connect()
+    except Exception as e:
+        print("Failed to connect")
+        print(e.get_dbus_name())
+        print(e.get_dbus_message())
+        if("UnknownObject" in e.get_dbus_name()):
+            print("Try scanning first to resolve this problem")
+        return bluetooth_constants.RESULT_EXCEPTION
+
+    else:
+        print("Connected OK")
+        return bluetooth_constants.RESULT_OK
+
+if(len(sys.argv) != 2):
+    print("usage: ./con_dis.py [bdaddr]")
+    sys.exit(1)
+
+dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+
+bdaddr = sys.argv[1]
+bus = dbus.SystemBus()
+adapter_path = bluetooth_constants.BLUEZ_NAMESPACE + bluetooth_constants.ADAPTER_NAME
+device_path = bluetooth_utils.device_address_to_path(bdaddr, adapter_path)
+device_proxy = bus.get_object(bluetooth_constants.BLUEZ_SERVICE_NAME, device_path)
+
+iface_prop = dbus.Interface(device_proxy, bluetooth_constants.DBUS_PROPERTIES) #интерфейс чтобы посмотреть свойства
+con = iface_prop.Get(bluetooth_constants.DEVICE_INTERFACE, 'Connected') #для Connected()
+
+device_interface = dbus.Interface(device_proxy, bluetooth_constants.DEVICE_INTERFACE)#интерфейс для конкретного устройства
+
+if con:
+    print('device is always connected')
+else:
+    print("Connecting to " + bdaddr)
+    res = connect()
+    if res == bluetooth_constants.RESULT_OK:
+        bus.add_signal_receiver(signal_received, dbus_interface = bluetooth_constants.DBUS_OM_IFACE, signal_name = "InterfacesAdded")
+        mainloop = GLib.MainLoop()
+        mainloop.run()
+
+
+print('----exit---')
+
